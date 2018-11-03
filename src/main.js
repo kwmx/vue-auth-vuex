bdd18facd46760c9f3ca4cec0cf52a383e4985ae
Faisal K <kwmx@users.noreply.github.com>
2018-11-03 03:23:57 -0400

    Fix the token storage name

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import Axios from 'axios'
 
 Vue.prototype.$http = Axios;
 
-const token = localStorage.getItem('user-token')
+const token = localStorage.getItem('token')
 if (token) {
   Vue.prototype.$http.defaults.headers.common['Authorization'] = token
 }
